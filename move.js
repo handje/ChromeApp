600acdc752e16615ca70556f38ae1592ff173758
Sonjieun <sonjieun2661@gmail.com>
2022-03-13 20:23:27 +0900

    login
    user name을 local storage에 저장

---
+++ move.js
@@ -0,0 +1,39 @@
+//root
+const USERNAME = "username"; //name의 key
+const HIDDEN = "hidden"; //class name
+
+//login input info
+const loginForm = document.querySelector("#login_form");
+const InputLogin = document.querySelector("#login_form input");
+
+//user name을 출력할 요소
+const helloUser = document.querySelector("#hello_User");
+
+//local storage 확인
+const userInfo = localStorage.getItem(USERNAME);
+
+//function
+//login
+function handleLogin(event) {
+  event.preventDefault();
+  const name = InputLogin.value;
+  loginForm.classList.add(HIDDEN);
+  localStorage.setItem(USERNAME, name);
+  showUserName(name);
+}
+
+//로그인한 user name 출력
+function showUserName(name) {
+  helloUser.innerText = `Hello ${name}!`;
+  helloUser.classList.remove(HIDDEN);
+}
+
+//main
+if (userInfo === null) {
+  //로그인 기록이 없을 경우
+  loginForm.classList.remove(HIDDEN);
+  loginForm.addEventListener("submit", handleLogin);
+} else {
+  //로그인 기록이 있을 경우
+  showUserName(userInfo);
+}
