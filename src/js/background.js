84d19df73e54d105f8bad6d79461ee722e6504c6
Sonjieun <sonjieun2661@gmail.com>
2022-04-08 20:46:13 +0900

    modify

--- src/js/background.js
+++ src/js/background.js
@@ -8,33 +8,6 @@ let index = image.indexOf(todayImage);
 const background = photo.querySelector("img");
 background.src = `src/img/${todayImage}`;
 
-const div = photo.querySelector(".photoBtn");
-const prev = document.createElement("button");
-prev.innerText = "prev";
-const next = document.createElement("button");
-next.innerText = "next";
-div.appendChild(prev); //html에 img tag 추가
-div.appendChild(next); //html에 img tag 추가
-
-prev.addEventListener("click", () => {
-  if (index - 1 >= 0) {
-    index--;
-    background.src = `src/img/${image[index]}`;
-  } else {
-    index = image.length - 1;
-    background.src = `src/img/${image[index]}`;
-  }
-});
-next.addEventListener("click", () => {
-  if (index + 1 < image.length) {
-    index++;
-    background.src = `src/img/${image[index]}`;
-  } else {
-    index = 0;
-    background.src = `src/img/${image[index]}`;
-  }
-});
-
 if (localStorage.getItem("username")) {
   photo.classList.remove("hidden");
 }
